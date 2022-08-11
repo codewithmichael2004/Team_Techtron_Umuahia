c8ce770b97a4228e451deb3c7af95fc2b320e9a4
udeh miracle tochukwu <miracletochukwu5@gmail.com>
2022-08-11 22:36:49 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
 
-window.onscroll = () =>{
+window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
 
+// console.log("home")
+
+const userName = window.prompt('Enter UserName');
+
+document.getElementById('userN').textContent = `Welcome ${userName}`;
 
 
+
+
+
+// Visitor Count Display 
+
+const countEl = document.getElementById('counter');
+
+visitorCountUpdate();
+
+function visitorCountUpdate() {
+    fetch('https://api.countapi.xyz/update/shopping/tech/?amount=1')
+        .then(res => res.json())
+        .then(res => {
+            countEl.innerHTML = res.value;
+        });
+
+    console.log(res.value);
+}
