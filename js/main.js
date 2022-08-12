602e8bda1e34e95614553f89f3aa684e839176a9
MICHAEL <92147287+codewithmichael2004@users.noreply.github.com>
2022-08-12 17:00:27 +0200

    Michael

--- js/main.js
+++ js/main.js
@@ -31,7 +31,6 @@ function visitorCountUpdate() {
 
     console.log(res.value);
 }
-
 // program to display the date and time
 const date = new Date();
 const day = date.toDateString();
@@ -40,21 +39,5 @@ const time = date.toLocaleTimeString();
 document.getElementById("date").textContent = day;
 
 document.getElementById("time").textContent = time;
-
-
-
-
-  let x = document.getElementById("location");
-
-  function showPosition() {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            let positionInfo = " (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
-            document.getElementById("location").innerHTML = positionInfo;
-        });
-    } else {
-        alert("Sorry, your browser does not support HTML5 geolocation.");
-    }
-}
 
 window.onload(showPosition());
